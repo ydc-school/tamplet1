a4530150caf31f3eb1c5ec3442be87ee33fb50bc
yo-dalo <kumaradarsh00572@gmail.com>
2026-03-15 15:59:37 +0530

    daynmic favicon

--- src/context/SchoolContext.jsx
+++ src/context/SchoolContext.jsx
@@ -2,28 +2,39 @@
 import { createContext, useContext, useState, useEffect } from "react";
 import axios from "axios";
 
-// ─── Context ───────────────────────────────────────────────────────────────────
 const SchoolContext = createContext(null);
 
-// ─── Provider ──────────────────────────────────────────────────────────────────
 export function SchoolProvider({ children }) {
     const [schoolInfo, setSchoolInfo] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
+    // favicon change
+    useEffect(() => {
+        if (!schoolInfo?.Favicon_Url) return;
+
+        let link = document.querySelector("link[rel='icon']");
+
+        if (!link) {
+            link = document.createElement("link");
+            link.rel = "icon";
+            document.head.appendChild(link);
+        }
+
+        const logoUrl = schoolInfo?.Favicon_Url
+            ? `/uploads/${schoolInfo.Favicon_Url}`
+            : "/logo/6.png";
+        link.href = logoUrl;
+
+    }, [schoolInfo]);
+
     useEffect(() => {
         const fetchSchoolInfo = async () => {
             try {
-                const response = await axios.get(
-                    "/api/client/school-info"
-                );
-                if (
-                    response.data.status === "success"
-                    //  &&
-                    // response.data.data.data.length > 0
-                ) {
-                    setSchoolInfo(response.data.data[0]);
+                const response = await axios.get("/api/client/school-info");
 
+                if (response.data.status === "success") {
+                    setSchoolInfo(response.data.data[0]);
                 }
             } catch (err) {
                 console.error("Error fetching school info:", err);
@@ -43,7 +54,6 @@ export function SchoolProvider({ children }) {
     );
 }
 
-// ─── Custom Hook ───────────────────────────────────────────────────────────────
 export function useSchool() {
     const context = useContext(SchoolContext);
     if (!context) {
