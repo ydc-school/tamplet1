a46e1f148bfd8ba747a30371d720cfb02190edd0
adarsh <kumaradarsh00572@gmail.com>
2026-06-23 12:19:43 +0530

    school  img add

--- src/components/home/Branch.jsx
+++ src/components/home/Branch.jsx
@@ -16,7 +16,7 @@ export default function Branch() {
                             <div className="aspect-video overflow-hidden">
                                 <img alt="Yaduvanshi College of Engg. & Tech., Narnaul (B.Tech., M.Tech.)"
                                     className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-500"
-                                    src="https://lh3.googleusercontent.com/aida/AP1WRLuYWg6mkZUcYCj0ivrrMv6vRv79NOafLF_WzQ-NCueSAtKVqPKuSrVXXetICX34XHo2S2mfJWmKq2cGtnXEKlI1NYR5ocl3tadeFG6vuvAJVYKYOGMdFxVI8hZyisi4FTBNzuUuiW3cOnzQgUY6wsGOiTvASTLJw_Lc7QCMnwuqsI7-7pmcQeuaoGuTb8rxfmDg6EWwdSVarLu4EN-WCfTJcV6SUdMS3odNB3QNhLK0RUGKGi-mAtQFuqI" />
+                                    src="https://admin.yaduvanshigroup.edu.in/uploads/branchs/1782195923309-9850066d31c04da9.jpeg" />
                             </div>
                             <div className="p-4 border-t-4 border-heritage-navy">
                                 <p className="font-label-caps text-label-caps text-heritage-navy">Yaduvanshi College of Engg. & Tech., Narnaul (B.Tech., M.Tech.)
@@ -28,7 +28,7 @@ export default function Branch() {
                             <div className="aspect-video overflow-hidden">
                                 <img alt="Yaduvandhi Degree College, Mahendergarh (UG, PG)"
                                     className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-500"
-                                    src="https://lh3.googleusercontent.com/aida/AP1WRLt2XP7RtCH3Js1TM9cdxftmHR00a4nWKlrppquRXbZ_HS86jAL06Nm0qcCl0-W6JBwEsDOT3YJF3_wyNlG1WJGiSdSkjGf1yKXSc9Sv1WK8I8v4-BDabIAlFyYIW3Z-YXJElpeYtROoRksHDEHt_GyQHyJd2ZEkvt2aVelWF5NpmrFbq9JaMmUTyBqwpXdgVDpGU8EL2PsDN6D9N2tNdj3nC_MbmvGZJtssEQ5hMM37HI4MPWC0jVCCVLw" />
+                                    src="https://admin.yaduvanshigroup.edu.in/uploads/branchs/1782195923309-9850066d31c04da9.jpeg" />
                             </div>
                             <div className="p-4 border-t-4 border-heritage-navy">
                                 <p className="font-label-caps text-label-caps text-heritage-navy">Yaduvandhi Degree College, Mahendergarh (UG, PG)
@@ -252,7 +252,7 @@ export default function Branch() {
                             <div className="aspect-video overflow-hidden">
                                 <img alt="Yaduvanshi Shiksha Niketan, MahenderGarh"
                                     className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-500"
-                                    src="https://lh3.googleusercontent.com/aida/AP1WRLuYWg6mkZUcYCj0ivrrMv6vRv79NOafLF_WzQ-NCueSAtKVqPKuSrVXXetICX34XHo2S2mfJWmKq2cGtnXEKlI1NYR5ocl3tadeFG6vuvAJVYKYOGMdFxVI8hZyisi4FTBNzuUuiW3cOnzQgUY6wsGOiTvASTLJw_Lc7QCMnwuqsI7-7pmcQeuaoGuTb8rxfmDg6EWwdSVarLu4EN-WCfTJcV6SUdMS3odNB3QNhLK0RUGKGi-mAtQFuqI" />
+                                    src="https://admin.yaduvanshigroup.edu.in/uploads/branchs/1782196595531-ed03fce9ad8d817e.jpeg" />
                             </div>
                             <div className="p-4 border-t-4 border-heritage-navy">
                                 <p className="font-label-caps text-label-caps text-heritage-navy">Yaduvanshi Shiksha Niketan, MahenderGarh
@@ -264,7 +264,7 @@ export default function Branch() {
                             <div className="aspect-video overflow-hidden">
                                 <img alt="Yaduvanshi Shiksha Niketan, Narnaul"
                                     className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-500"
-                                    src="https://lh3.googleusercontent.com/aida/AP1WRLt2XP7RtCH3Js1TM9cdxftmHR00a4nWKlrppquRXbZ_HS86jAL06Nm0qcCl0-W6JBwEsDOT3YJF3_wyNlG1WJGiSdSkjGf1yKXSc9Sv1WK8I8v4-BDabIAlFyYIW3Z-YXJElpeYtROoRksHDEHt_GyQHyJd2ZEkvt2aVelWF5NpmrFbq9JaMmUTyBqwpXdgVDpGU8EL2PsDN6D9N2tNdj3nC_MbmvGZJtssEQ5hMM37HI4MPWC0jVCCVLw" />
+                                    src="https://admin.yaduvanshigroup.edu.in/uploads/branchs/1782196658975-60154148faf9770c.jpeg" />
                             </div>
                             <div className="p-4 border-t-4 border-heritage-navy">
                                 <p className="font-label-caps text-label-caps text-heritage-navy">Yaduvanshi Shiksha Niketan, Narnaul
@@ -276,7 +276,7 @@ export default function Branch() {
                             <div className="aspect-video overflow-hidden">
                                 <img alt="Yaduvanshi Shiksha Niketan, Nangal Chaudhary"
                                     className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-500"
-                                    src="https://lh3.googleusercontent.com/aida/AP1WRLtku6SoK7BEK_XG9Y84QxQbk0lGL6XdMwbaMbMplVK-iUwZN5rjQv4ni-6YRlAOICQfWpVf1FD7lVI-Gm9KN2cT4GhiMhwf0Gtx9rGhOcu4WBiEBEAAHJpA6UE_SvG5Lctt51c2btkjOnSAb2lPg_KxOQU0RjSMWZgnTTKSB3YdREnQHNk8GjJYnhmS0Gbfyp1VIsqrdZILNFIp5Sgx0gOZzXzGYed3rJoC4ajmMfog5m4Rn3hnJNcz6A" />
+                                    src="" />
                             </div>
                             <div className="p-4 border-t-4 border-heritage-navy">
                                 <p className="font-label-caps text-label-caps text-heritage-navy">Yaduvanshi Shiksha Niketan, Nangal Chaudhary</p>
@@ -287,7 +287,7 @@ export default function Branch() {
                             <div className="aspect-video overflow-hidden">
                                 <img alt="Yaduvanshi Shiksha Niketan, Satnali"
                                     className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-500"
-                                    src="https://lh3.googleusercontent.com/aida/AP1WRLuV8GkQDIEmQw_rSOHMAbd5Du7b4wXj1J8oMXwcrobFuLZJCeuu3LVzQe1UZyKMXQ6tfl6-AxlC0u_K_TvKC2wedNt8J8c16cPK7cpSa5cqoJ-HjG9tfApYjsrK4TPdwHyj42gQQbVqk61HAuzUgU1_J0NALec3gVrkYEmC5HbZiDD-QGNQB6x6-3F64nR318dzh2XmuHkt6s4quS9F9QE-Fo-fV4fyAIQjfLDXm9DpjQgY2YrlK8qXb_4" />
+                                    src="https://admin.yaduvanshigroup.edu.in/uploads/branchs/1782196860315-7e01b30c0b6157e0.jpeg" />
                             </div>
                             <div className="p-4 border-t-4 border-heritage-navy">
                                 <p className="font-label-caps text-label-caps text-heritage-navy">Yaduvanshi Shiksha Niketan, Satnali</p>
@@ -298,7 +298,7 @@ export default function Branch() {
                             <div className="aspect-video overflow-hidden">
                                 <img alt="Yaduvanshi Shiksha Niketan, Kanina"
                                     className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-500"
-                                    src="https://lh3.googleusercontent.com/aida/AP1WRLtrdK4T4kjAfzAKE3gYdmh0unXnwXPL2jdO5-1UDBp-zgRg9Ue9M_62nv4LU-iFIOZ_p7z-Bgn9zCMgvNno3Ac7ZUo_A3UlyRDzfy9JSYLMNwLe70xIdzpl7GaDw_xjiAEFLZk4OhEjkfxaS7FOblB5Pr4zk7pLmMkKrWFJZqNTyiJlUOgDyo-W0g0SNiMLAnEmZ-UYSrMe-_TIvFbV5wsVS9FvmZkYQaGF2sGkOwuEi4UI4Hz8kIt6tCs" />
+                                    src="https://admin.yaduvanshigroup.edu.in/uploads/branchs/1782196903986-6f37a91ea40646af.jpeg" />
                             </div>
                             <div className="p-4 border-t-4 border-heritage-navy">
                                 <p className="font-label-caps text-label-caps text-heritage-navy">Yaduvanshi Shiksha Niketan, Kanina</p>
@@ -309,7 +309,7 @@ export default function Branch() {
                             <div className="aspect-video overflow-hidden">
                                 <img alt="Yaduvanshi Shiksha Niketan, Rewari"
                                     className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-500"
-                                    src="https://lh3.googleusercontent.com/aida/AP1WRLu-CCSbkK2rOiTQk_dYaYYeo_opJYK6A8zmLTnsbO_mZg155Atu5z8d01tGa5U6G7iwf--okhdbbbGimzgughFVED85qwa3gnyc8cODsu8u3uBEBLnGEUnOz4sAy_1CZe9Vf8Xsy8yAyaLi_CbrdRfXER3pjATQ1jtPnxXdCKIAGRj532XnBqzQruscbyQ_q-XojegUVlliTdeF6eMJ0mgC-fwm1sq4HzU71s5b5tvEhMGeoj07vQyhadU" />
+                                    src="https://admin.yaduvanshigroup.edu.in/uploads/branchs/1782196982829-2596b7d1b38ea274.jpeg" />
                             </div>
                             <div className="p-4 border-t-4 border-heritage-navy">
                                 <p className="font-label-caps text-label-caps text-heritage-navy">Yaduvanshi Shiksha Niketan, Rewari
@@ -321,7 +321,7 @@ export default function Branch() {
                             <div className="aspect-video overflow-hidden">
                                 <img alt="Yaduvanshi Shiksha Niketan, Kosli"
                                     className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-500"
-                                    src="https://lh3.googleusercontent.com/aida/AP1WRLuX2iUaoWUVDx4MCECZ1zZ8eCbWqLIcouk_JK8jWgVZ7rz2srC4g7NZu5ufVJ_NOGjgsDzqCaZz8MiOgf9uPFz8jr-qsyA5MDvdLIPuzwKhGicwqFwCvM3SVhdeH2W34SJPiSO42ZgDAZNnBNFagDU8A2faTvDJuIfwN6pjRo-lu5349hfaNwVhKjdyHwhxrpaXllFNoVnSeBSzoUBbNk7ysgpzP2UV4ge5oL09GIIW7iMbbMdYpzupk-Y" />
+                                    src="https://admin.yaduvanshigroup.edu.in/uploads/branchs/1782197050002-2b8495d52b87ee98.jpeg" />
                             </div>
                             <div className="p-4 border-t-4 border-heritage-navy">
                                 <p className="font-label-caps text-label-caps text-heritage-navy">Yaduvanshi Shiksha Niketan, Kosli</p>
@@ -332,7 +332,7 @@ export default function Branch() {
                             <div className="aspect-video overflow-hidden">
                                 <img alt="Yaduvanshi Shiksha Niketan, Gurugram Sec-33"
                                     className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-500"
-                                    src="https://lh3.googleusercontent.com/aida/AP1WRLu1HcY5kndBhVN9aKo4LR_xAZYGg1R2yaVk1kQR_8V05d-6Gj0BLZHOtgN7z4wnIBpWvCkG7kYENsR7tZhnosKrfaQpfbHNnnTzqjx5VB1rfqFuzjRewfnHsqdO1iXkNo5XpGTanJLOecpyKLiEWZidxEk1UBeNA529E6kUpUIyWf2JsfGEtFAcmCdZAvW99bk-UJEmCuJ40uUk3FUPSaCdwZzvfQ9xKqbN73RAMXR2NjaeHFdhc8sqtg" />
+                                    src="https://admin.yaduvanshigroup.edu.in/uploads/branchs/1782197114404-418695cc18b74268.jpeg" />
                             </div>
                             <div className="p-4 border-t-4 border-heritage-navy">
                                 <p className="font-label-caps text-label-caps text-heritage-navy">Yaduvanshi Shiksha Niketan, Gurugram Sec-33</p>
@@ -343,7 +343,7 @@ export default function Branch() {
                             <div className="aspect-video overflow-hidden">
                                 <img alt="Yaduvanshi Shiksha Niketan, Gurugram Sec-92"
                                     className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-500"
-                                    src="https://lh3.googleusercontent.com/aida/AP1WRLuX2iUaoWUVDx4MCECZ1zZ8eCbWqLIcouk_JK8jWgVZ7rz2srC4g7NZu5ufVJ_NOGjgsDzqCaZz8MiOgf9uPFz8jr-qsyA5MDvdLIPuzwKhGicwqFwCvM3SVhdeH2W34SJPiSO42ZgDAZNnBNFagDU8A2faTvDJuIfwN6pjRo-lu5349hfaNwVhKjdyHwhxrpaXllFNoVnSeBSzoUBbNk7ysgpzP2UV4ge5oL09GIIW7iMbbMdYpzupk-Y" />
+                                    src="https://admin.yaduvanshigroup.edu.in/uploads/branchs/1782197147934-0581a80a28047452.jpeg" />
                             </div>
                             <div className="p-4 border-t-4 border-heritage-navy">
                                 <p className="font-label-caps text-label-caps text-heritage-navy">Yaduvanshi Shiksha Niketan, Gurugram Sec-92</p>
@@ -354,7 +354,7 @@ export default function Branch() {
                             <div className="aspect-video overflow-hidden">
                                 <img alt="Yaduvanshi Shiksha Niketan, Charkhi Dadri"
                                     className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-500"
-                                    src="https://lh3.googleusercontent.com/aida/AP1WRLuX2iUaoWUVDx4MCECZ1zZ8eCbWqLIcouk_JK8jWgVZ7rz2srC4g7NZu5ufVJ_NOGjgsDzqCaZz8MiOgf9uPFz8jr-qsyA5MDvdLIPuzwKhGicwqFwCvM3SVhdeH2W34SJPiSO42ZgDAZNnBNFagDU8A2faTvDJuIfwN6pjRo-lu5349hfaNwVhKjdyHwhxrpaXllFNoVnSeBSzoUBbNk7ysgpzP2UV4ge5oL09GIIW7iMbbMdYpzupk-Y" />
+                                    src="https://admin.yaduvanshigroup.edu.in/uploads/branchs/1782195923309-9850066d31c04da9.jpeg" />
                             </div>
                             <div className="p-4 border-t-4 border-heritage-navy">
                                 <p className="font-label-caps text-label-caps text-heritage-navy">Yaduvanshi Shiksha Niketan, Charkhi Dadri</p>
@@ -365,7 +365,7 @@ export default function Branch() {
                             <div className="aspect-video overflow-hidden">
                                 <img alt="Yaduvanshi Shiksha Niketan, Hansi"
                                     className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-500"
-                                    src="https://lh3.googleusercontent.com/aida/AP1WRLuX2iUaoWUVDx4MCECZ1zZ8eCbWqLIcouk_JK8jWgVZ7rz2srC4g7NZu5ufVJ_NOGjgsDzqCaZz8MiOgf9uPFz8jr-qsyA5MDvdLIPuzwKhGicwqFwCvM3SVhdeH2W34SJPiSO42ZgDAZNnBNFagDU8A2faTvDJuIfwN6pjRo-lu5349hfaNwVhKjdyHwhxrpaXllFNoVnSeBSzoUBbNk7ysgpzP2UV4ge5oL09GIIW7iMbbMdYpzupk-Y" />
+                                    src="https://admin.yaduvanshigroup.edu.in/uploads/branchs/1782197223758-8f82ad88c8da3697.jpeg" />
                             </div>
                             <div className="p-4 border-t-4 border-heritage-navy">
                                 <p className="font-label-caps text-label-caps text-heritage-navy">Yaduvanshi Shiksha Niketan, Hansi</p>
@@ -376,7 +376,7 @@ export default function Branch() {
                             <div className="aspect-video overflow-hidden">
                                 <img alt="Yaduvanshi Shiksha Niketan, Jind"
                                     className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-500"
-                                    src="https://lh3.googleusercontent.com/aida/AP1WRLuX2iUaoWUVDx4MCECZ1zZ8eCbWqLIcouk_JK8jWgVZ7rz2srC4g7NZu5ufVJ_NOGjgsDzqCaZz8MiOgf9uPFz8jr-qsyA5MDvdLIPuzwKhGicwqFwCvM3SVhdeH2W34SJPiSO42ZgDAZNnBNFagDU8A2faTvDJuIfwN6pjRo-lu5349hfaNwVhKjdyHwhxrpaXllFNoVnSeBSzoUBbNk7ysgpzP2UV4ge5oL09GIIW7iMbbMdYpzupk-Y" />
+                                    src="https://admin.yaduvanshigroup.edu.in/uploads/branchs/1782197281143-cd38ea07ac981a01.jpeg" />
                             </div>
                             <div className="p-4 border-t-4 border-heritage-navy">
                                 <p className="font-label-caps text-label-caps text-heritage-navy">Yaduvanshi Shiksha Niketan, Jind</p>
