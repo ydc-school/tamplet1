a15e1b23faa02de53f6b308f9745b6ccaf68714b
adarsh <kumaradarsh00572@gmail.com>
2026-06-26 18:47:14 +0530

    .

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -53,7 +53,7 @@ export default function Navbar() {
         <div className="flex justify-between items-center px-gutter py-8 w-full max-w-container-max mx-auto">
 
           {/* लोगो सेक्शन */}
-          <div className="flex items-center gap-4">
+          <div className="flex items-center flex-col gap-4">
             <Link href="/">
               <img
                 alt="School Logo"
@@ -62,6 +62,23 @@ export default function Navbar() {
                 onError={handleLogoError}
               />
             </Link>
+            <h1  style={{}}  className="text-4xl  lg:hidden font-extrabold flex items-center gap-3">
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="w-9 h-9 text-current" /* Aapki current text color ke sath match ho jayega */
+              >
+                <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
+              </svg>
+
+              <span>+91-9729429766</span>
+            </h1>
           </div>
 
 
@@ -108,9 +125,8 @@ export default function Navbar() {
 
 
 
+          <h1 className="text-4xl lg:flex  font-extrabold hidden justify-center items-center gap-3">
 
-          <h1 className="text-4xl font-extrabold flex items-center gap-3">
-           
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -126,6 +142,7 @@ export default function Navbar() {
 
             <span>+91-9729429766</span>
           </h1>
+
 
 
 
