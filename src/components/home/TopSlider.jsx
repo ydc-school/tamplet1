79c4d79e229f3b45f82b9faa068e8b6bff73f9bc
adarsh <kumaradarsh00572@gmail.com>
2026-06-26 17:21:11 +0530

    .

--- src/components/home/TopSlider.jsx
+++ src/components/home/TopSlider.jsx
@@ -242,123 +242,126 @@ export default function TopSlider() {
 
 
 
-<div className="max-w-screen p-8">
+      <div className="max-w-screen p-8">
 
-    <div className="flex justify-center mb-8">
-        <div className="bg-blue-800 text-white px-12 py-3 rounded-xl text-3xl font-bold shadow-lg">
+        <div className="flex justify-center mb-8">
+          <div className="bg-blue-800 text-white px-12 py-3 rounded-xl text-3xl font-bold shadow-lg">
             COURSES OFFERED
+          </div>
         </div>
-    </div>
 
-    <div className="grid lg:grid-cols-5 grid-cols-1 gap-6">
-
-      
-
-       
+        <div class="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-4">
 
 
 
-<div className="col-span-2">
+
+
+
+          <div className="col-span-full">
 
             <div className="bg-red-600 text-white text-center py-2 rounded-xl font-bold mb-4">
-                PG Courses
+              PG Courses
             </div>
 
-            <div className="grid grid-cols-2 gap-2">
+            <div className="grid grid-cols-2 gap-2 w-full">
 
-                <div className="space-y-2">
+              <div className="space-y-2">
 
-                      <div className="bg-cyan-400 rounded-lg px-2 whitespace-nowrap text-center font-semibold">M.Tech (CSE)</div>
-                    <div className="bg-cyan-100 rounded-lg p-2 text-center font-semibold">M.Tech (ECE)</div>
-                    <div className="bg-cyan-400 rounded-lg p-2 text-center font-semibold">M.Ed.</div>
-                    <div className="bg-cyan-100 rounded-lg p-2 text-center font-semibold">M.A. Hindi</div>
-                    <div className="bg-cyan-400 rounded-lg p-2 text-center font-semibold">M.A. English</div>
-                    <div className="bg-cyan-100 rounded-lg p-2 text-center font-semibold">M.A. Sanskrit</div>
-                    <div className="bg-cyan-400 rounded-lg p-2 text-center font-semibold">M.A. History</div>
-                    <div className="bg-cyan-100 rounded-lg p-2 text-center font-semibold">M.A. Political Science</div>
+                <div className="bg-cyan-400 rounded-lg p-2 text-center font-semibold">M.Tech (CSE)</div>
+                <div className="bg-cyan-100 rounded-lg p-2 text-center font-semibold">M.Tech (ECE)</div>
+                <div className="bg-cyan-400 rounded-lg p-2 text-center font-semibold">M.Ed.</div>
+                <div className="bg-cyan-100 rounded-lg p-2 text-center font-semibold">M.A. Hindi</div>
+                <div className="bg-cyan-400 rounded-lg p-2 text-center font-semibold">M.A. English</div>
+                <div className="bg-cyan-100 rounded-lg p-2 text-center font-semibold">M.A. Sanskrit</div>
+                <div className="bg-cyan-400 rounded-lg p-2 text-center font-semibold">M.A. History</div>
+                <div className="bg-cyan-100 rounded-lg p-2 text-center font-semibold">M.A. Political Science</div>
 
-                </div>
+              </div>
 
-                <div className="space-y-2">
-
-                 
-                    <div className="bg-cyan-400 rounded-lg p-2 text-center font-semibold">M.Com</div>
-                    <div className="bg-cyan-100 rounded-lg p-2 text-center font-semibold">M.Sc Physics</div>
-                    <div className="bg-cyan-400 rounded-lg p-2 text-center font-semibold">M.Sc Chemistry</div>
-                    <div className="bg-cyan-100 rounded-lg p-2 text-center font-semibold">M.Sc Maths</div>
-                    <div className="bg-cyan-400 rounded-lg p-2 text-center font-semibold">M.Sc Zoology</div>
-                    <div className="bg-cyan-100 rounded-lg p-2 text-center font-semibold">M.Sc Botany</div>
-                    <div className="bg-cyan-400 rounded-lg p-2 text-center font-semibold">M.Sc Computer Sci.</div>
-                    <div className="bg-cyan-100 rounded-lg p-2 text-center font-semibold">M.Sc Geography</div>
-
-                </div>
-
-            </div>
-
-        </div>
+              <div className="space-y-2">
 
 
+                <div className="bg-cyan-400 rounded-lg p-2 text-center font-semibold">M.Com</div>
+                <div className="bg-cyan-100 rounded-lg p-2 text-center font-semibold">M.Sc Physics</div>
+                <div className="bg-cyan-400 rounded-lg p-2 text-center font-semibold">M.Sc Chemistry</div>
+                <div className="bg-cyan-100 rounded-lg p-2 text-center font-semibold">M.Sc Maths</div>
+                <div className="bg-cyan-400 rounded-lg p-2 text-center font-semibold">M.Sc Zoology</div>
+                <div className="bg-cyan-100 rounded-lg p-2 text-center font-semibold">M.Sc Botany</div>
+                <div className="bg-cyan-400 rounded-lg p-2 text-center font-semibold">M.Sc Computer Sci.</div>
+                <div className="bg-cyan-100 rounded-lg p-2 text-center font-semibold">M.Sc Geography</div>
 
-        <div className="col-span-2">
-
-            <div className="bg-red-600 text-white text-center py-2 rounded-xl font-bold mb-4">
-                UG Courses
-            </div>
-
-            <div className="grid grid-cols-3 gap-2">
-
-                <div className="space-y-2">
-
-                    <div className="bg-yellow-400 rounded-lg p-2 text-center font-semibold">B.Tech (CSE)</div>
-                    <div className="bg-yellow-100 rounded-lg p-2 text-center font-semibold">B.Tech (CE)</div>
-                    <div className="bg-yellow-400 rounded-lg p-2 text-center font-semibold">B.Tech (ME)</div>
-                    <div className="bg-yellow-100 rounded-lg p-2 text-center font-semibold">B.Tech (ECE)</div>
-                    <div className="bg-yellow-400 rounded-lg p-2 text-center font-semibold">B.Tech (EE)</div>
-                    <div className="bg-yellow-100 rounded-lg p-2 text-center font-semibold">B.Ed.</div>
-                    <div className="bg-yellow-400 rounded-lg p-2 text-center font-semibold">B.A.-B.Ed.</div>
-                    <div className="bg-yellow-100 rounded-lg p-2 text-center font-semibold">B.Sc.-B.Ed.</div>
-                    <div className="bg-yellow-400 rounded-lg p-2 text-center font-semibold">B.P.Ed.</div>
-
-                </div>
-
-                <div className="space-y-2">
-
-                    <div className="bg-yellow-400 rounded-lg p-2 text-center font-semibold">B.A.</div>
-                    <div className="bg-yellow-100 rounded-lg p-2 text-center font-semibold">B.A. Yoga</div>
-                    <div className="bg-yellow-400 rounded-lg p-2 text-center font-semibold">B.A. Hons English</div>
-                    <div className="bg-yellow-100 rounded-lg p-2 text-center font-semibold">B.Com</div>
-                    <div className="bg-yellow-400 rounded-lg p-2 text-center font-semibold">B.Com Hons</div>
-                    <div className="bg-yellow-100 rounded-lg p-2 text-center font-semibold">B.Sc Medical</div>
-                    <div className="bg-yellow-400 rounded-lg p-2 text-center font-semibold">B.Sc Non Medical</div>
-                    <div className="bg-yellow-100 rounded-lg p-2 text-center font-semibold">B.Sc Hons Physics</div>
-                    <div className="bg-yellow-400 rounded-lg p-2 text-center font-semibold">B.Sc Hons Chemistry</div>
-
-                </div>
-
-                <div className="space-y-2">
-
-                    <div className="bg-yellow-400 rounded-lg p-2 text-center font-semibold">B.Sc Hons Maths</div>
-                    <div className="bg-yellow-100 rounded-lg p-2 text-center font-semibold">B.Sc Hons Zoology</div>
-                    <div className="bg-yellow-400 rounded-lg p-2 text-center font-semibold">B.Sc Hons Botany</div>
-                    <div className="bg-yellow-100 rounded-lg p-2 text-center font-semibold">B.Sc Computer Sci.</div>
-                    <div className="bg-yellow-400 rounded-lg p-2 text-center font-semibold">BBA</div>
-                    <div className="bg-yellow-100 rounded-lg p-2 text-center font-semibold">BCA</div>
-
-                </div>
+              </div>
 
             </div>
 
-        </div>
+          </div>
 
-      
 
-        <div>
+
+          <div className="col-span-full">
 
             <div className="bg-red-600 text-white text-center py-2 rounded-xl font-bold mb-4">
               UG Courses
             </div>
 
-            <div className="space-y-2">
+            <div className="grid grid-cols-3 gap-2 w-full">
+
+              <div className="col-w-full space-y-2">
+
+                <div className="bg-yellow-400 rounded-lg p-2 text-center font-semibold">B.Tech (CSE)</div>
+                <div className="bg-yellow-100 rounded-lg p-2 text-center font-semibold">B.Tech (CE)</div>
+                <div className="bg-yellow-400 rounded-lg p-2 text-center font-semibold">B.Tech (ME)</div>
+                <div className="bg-yellow-100 rounded-lg p-2 text-center font-semibold">B.Tech (ECE)</div>
+                <div className="bg-yellow-400 rounded-lg p-2 text-center font-semibold">B.Tech (EE)</div>
+                <div className="bg-yellow-100 rounded-lg p-2 text-center font-semibold">B.Ed.</div>
+                <div className="bg-yellow-400 rounded-lg p-2 text-center font-semibold">B.A.-B.Ed.</div>
+                <div className="bg-yellow-100 rounded-lg p-2 text-center font-semibold">B.Sc.-B.Ed.</div>
+                <div className="bg-yellow-400 rounded-lg p-2 text-center font-semibold">B.P.Ed.</div>
+
+              </div>
+
+              <div className="col-w-full space-y-2">
+
+                <div className="bg-yellow-400 rounded-lg p-2 text-center font-semibold">B.A.</div>
+                <div className="bg-yellow-100 rounded-lg p-2 text-center font-semibold">B.A. Yoga</div>
+                <div className="bg-yellow-400 rounded-lg p-2 text-center font-semibold">B.A. Hons English</div>
+                <div className="bg-yellow-100 rounded-lg p-2 text-center font-semibold">B.Com</div>
+                <div className="bg-yellow-400 rounded-lg p-2 text-center font-semibold">B.Com Hons</div>
+                <div className="bg-yellow-100 rounded-lg p-2 text-center font-semibold">B.Sc Medical</div>
+                <div className="bg-yellow-400 rounded-lg p-2 text-center font-semibold">B.Sc Non Medical</div>
+                <div className="bg-yellow-100 rounded-lg p-2 text-center font-semibold">B.Sc Hons Physics</div>
+                <div className="bg-yellow-400 rounded-lg p-2 text-center font-semibold">B.Sc Hons Chemistry</div>
+
+              </div>
+
+              <div className="col-w-full span-1">
+
+                <div className="bg-yellow-400 rounded-lg p-2 text-center font-semibold">B.Sc Hons Maths</div>
+                <div className="bg-yellow-100 rounded-lg p-2 text-center font-semibold">B.Sc Hons Zoology</div>
+                <div className="bg-yellow-400 rounded-lg p-2 text-center font-semibold">B.Sc Hons Botany</div>
+                <div className="bg-yellow-100 rounded-lg p-2 text-center font-semibold">B.Sc Computer Sci.</div>
+                <div className="bg-yellow-400 rounded-lg p-2 text-center font-semibold">BBA</div>
+                <div className="bg-yellow-100 rounded-lg p-2 text-center font-semibold">BCA</div>
+
+              </div>
+
+            </div>
+
+          </div>
+
+
+
+
+          <div className="col-span-full">
+
+            <div className="bg-red-600 text-white text-center py-2 rounded-xl font-bold mb-4">
+              Diploma Courses
+            </div>
+
+            <div className="grid grid-cols-1 gap-2 w-full">
+
+              <div className="min-w-full space-y-2">
+
 
                 <div className="bg-lime-400 rounded-lg p-2 text-center font-semibold">Polytechnic (ME)</div>
                 <div className="bg-lime-100 rounded-lg p-2 text-center font-semibold">Polytechnic (ECE)</div>
@@ -370,73 +373,108 @@ export default function TopSlider() {
                 <div className="bg-lime-100 rounded-lg p-2 text-center font-semibold">D.El.Ed.</div>
                 <div className="bg-lime-400 rounded-lg p-2 text-center font-semibold">D.P.Ed.</div>
 
+
+              </div>
+
+
+
             </div>
 
-        </div>
+          </div>
 
-       
-
-        <div>
-
+          <div className="col-span-full">
             <div className="bg-red-600 text-white text-center py-2 rounded-xl font-bold mb-4">
-                Distance Courses
+              Distance Courses
             </div>
 
-            <div className="grid grid-cols-2 gap-2">
+            <div className="grid grid-cols-2 gap-2 w-full">
 
-                <div className="space-y-2">
-                    <div className="bg-yellow-300 rounded-lg p-2 text-center font-semibold">M.Com</div>
-                    <div className="bg-yellow-300 rounded-lg p-2 text-center font-semibold">M.Sc Maths</div>
-                    <div className="bg-yellow-300 rounded-lg p-2 text-center font-semibold">B.A.</div>
-                </div>
+              <div className="col-span-w-full space-y-2">
+                <div className="bg-yellow-300 rounded-lg p-2 text-center font-semibold">M.Com</div>
+                <div className="bg-yellow-300 rounded-lg p-2 text-center font-semibold">M.Sc Maths</div>
+                <div className="bg-yellow-300 rounded-lg p-2 text-center font-semibold">B.A.</div>
+              </div>
 
-                <div className="space-y-2">
-                    <div className="bg-yellow-300 rounded-lg p-2 text-center font-semibold">M.A. English</div>
-                    <div className="bg-yellow-300 rounded-lg p-2 text-center font-semibold">M.A. Hindi</div>
-                    <div className="bg-yellow-300 rounded-lg p-2 text-center font-semibold">B.Com</div>
-                </div>
+              <div className="col-span-w-full space-y-2">
+                <div className="bg-yellow-300 rounded-lg p-2 text-center font-semibold">M.A. English</div>
+                <div className="bg-yellow-300 rounded-lg p-2 text-center font-semibold">M.A. Hindi</div>
+                <div className="bg-yellow-300 rounded-lg p-2 text-center font-semibold">B.Com</div>
+              </div>
 
             </div>
 
             <div className="bg-red-600 text-white text-center py-2 rounded-xl font-bold mt-6 mb-4">
-                Diploma Courses
+              Diploma Courses
             </div>
 
-            <div className="space-y-2">
+            <div className="col-span-w-full space-y-2">
 
-                <div className="bg-yellow-300 rounded-lg p-2 text-center font-semibold">Computer Applications & Data Science</div>
-                <div className="bg-yellow-300 rounded-lg p-2 text-center font-semibold">Artificial Intelligence</div>
-                <div className="bg-yellow-300 rounded-lg p-2 text-center font-semibold">Cyber Security</div>
-                <div className="bg-yellow-300 rounded-lg p-2 text-center font-semibold">Business Analytics</div>
-                <div className="bg-yellow-300 rounded-lg p-2 text-center font-semibold">Banking & Finance</div>
-                <div className="bg-yellow-300 rounded-lg p-2 text-center font-semibold">Financial Market</div>
+              <div className="bg-yellow-300 rounded-lg p-2 text-center font-semibold">Computer Applications & Data Science</div>
+              <div className="bg-yellow-300 rounded-lg p-2 text-center font-semibold">Artificial Intelligence</div>
+              <div className="bg-yellow-300 rounded-lg p-2 text-center font-semibold">Cyber Security</div>
+              <div className="bg-yellow-300 rounded-lg p-2 text-center font-semibold">Business Analytics</div>
+              <div className="bg-yellow-300 rounded-lg p-2 text-center font-semibold">Banking & Finance</div>
+              <div className="bg-yellow-300 rounded-lg p-2 text-center font-semibold">Financial Market</div>
 
             </div>
+
+          </div>
 
         </div>
 
-    </div>
+
+<div className="flex flex-col justify-center gap-5">
+
+<div className="flex flex ">
+
+
 
 </div>
 
 
 
 
-    
-
-  
 
 
 
 
 
 
+</div>
 
 
 
 
 
 
-        </>
-        );
+
+
+
+
+
+
+
+
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </>
+  );
 }
