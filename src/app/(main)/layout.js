e045c07f11f58b87800af5242eb83ed0e519619e
adarsh <kumaradarsh00572@gmail.com>
2026-06-29 10:26:39 +0530

    pop add kiya h

--- src/app/(main)/layout.js
+++ src/app/(main)/layout.js
@@ -1,12 +1,14 @@
 import Footer from "@/components/layout/Footer";
 import Navbar from "@/components/layout/Navbar";
 import Popup from "@/components/layout/Popup";
+import { Tpopup } from "@/components/layout/Tpopup";
 
 
 export default function MainLayout({ children }) {
   return (
     <>
      <Popup />
+     <Tpopup />
       <main className="overflow-hidden">
       
         <Navbar />
