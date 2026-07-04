7acd3b05d10b3b6632b3e7f75fddd138563a30b4
adarsh <kumaradarsh00572@gmail.com>
2026-07-04 13:38:35 +0530

    .

--- src/components/home/Achievementgallery.jsx
+++ src/components/home/Achievementgallery.jsx
@@ -46,55 +46,41 @@ export default function AchievementGallery() {
     },
   ];
 
-
-
-
   return (
     <>
+      <style>{`
+        .marquee-inner {
+            animation: marqueeScroll linear infinite;
+        }
 
-      <>
-        <style>{`
-                .marquee-inner {
-                    animation: marqueeScroll linear infinite;
-                }
+        @keyframes marqueeScroll {
+            0% {
+                transform: translateX(0%);
+            }
+            100% {
+                transform: translateX(-50%);
+            }
+        }
+      `}</style>
 
-                @keyframes marqueeScroll {
-                    0% {
-                        transform: translateX(0%);
-                    }
-
-                    100% {
-                        transform: translateX(-50%);
-                    }
-                }
-            `}</style>
-
-        <div className="overflow-hidden py-8 w-full relative max-w-6xl mx-auto" onMouseEnter={() => setStopScroll(true)} onMouseLeave={() => setStopScroll(false)}>
-          <div className="absolute left-0 top-0 h-full w-20 z-10 pointer-events-none bg-gradient-to-r from-white to-transparent" />
-          <div className="marquee-inner flex w-fit" style={{ animationPlayState: stopScroll ? "paused" : "running", animationDuration: cardData.length * 2500 + "ms" }}>
-            <div className="flex">
-              {achievements?.map((card, index) => (
-                <div key={index} className="min-w-56 mx-4 h-[20rem] relative group hover:scale-90 transition-all duration-300">
-                  <img src={`/uploads/${card?.Image}`} alt="card" className="w-full h-full object-fill" />
-                  <div className="flex items-center justify-center px-4 opacity-0 group-hover:opacity-100 transition-all duration-300 absolute bottom-0 backdrop-blur-md left-0 w-full h-full bg-black/20">
-                    <p className="text-white text-lg font-semibold text-center">
-                      {card?.Name}
-                    </p>
-                  </div>
+      <div className="overflow-hidden py-8 w-full relative max-w-6xl mx-auto" onMouseEnter={() => setStopScroll(true)} onMouseLeave={() => setStopScroll(false)}>
+        <div className="absolute left-0 top-0 h-full w-20 z-10 pointer-events-none bg-gradient-to-r from-white to-transparent" />
+        <div className="marquee-inner flex w-fit" style={{ animationPlayState: stopScroll ? "paused" : "running", animationDuration: cardData.length * 2500 + "ms" }}>
+          <div className="flex">
+            {achievements?.map((card, index) => (
+              <div key={index} className="w-72 mx-4 h-[20rem] relative group hover:scale-95 transition-all duration-300 flex-shrink-0">
+                <img src={`/uploads/${card?.Image}`} alt="card" className="w-full h-full object-contain" />
+                <div className="flex items-center justify-center px-4 opacity-0 group-hover:opacity-100 transition-all duration-300 absolute bottom-0 backdrop-blur-md left-0 w-full h-full bg-black/20">
+                  <p className="text-white text-lg font-semibold text-center">
+                    {card?.Name}
+                  </p>
                 </div>
-              ))}
-            </div>
+              </div>
+            ))}
           </div>
-          <div className="absolute right-0 top-0 h-full w-20 md:w-40 z-10 pointer-events-none bg-gradient-to-l from-white to-transparent" />
         </div>
-      </>
-
-
-
-
-
-
-
+        <div className="absolute right-0 top-0 h-full w-20 md:w-40 z-10 pointer-events-none bg-gradient-to-l from-white to-transparent" />
+      </div>
     </>
   );
 }
