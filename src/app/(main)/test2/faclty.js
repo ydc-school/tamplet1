5d15603a715dc946bf80c9c88bee0f1c791bb00a
adarsh <kumaradarsh00572@gmail.com>
2026-09-03 11:26:09 +0530

    g,l

---
+++ src/app/(main)/test2/faclty.js
@@ -0,0 +1,260 @@
+const json =
+    [{
+        "college": {
+            "name": "YADUVANSHI GROUP OF COLLEGE",
+            "location": "MAHENDERGARH, COLLEGE",
+            "type": "Faculty Directory"
+        },
+        "faculty": [
+            {
+                "employeeNumber": 13,
+                "name": "Mamta",
+                "department": "Physics",
+                "designation": "Assistant Professor"
+            },
+            {
+                "employeeNumber": 19,
+                "name": "Bhajanlal",
+                "department": "Physics",
+                "designation": "Assistant Professor"
+            },
+            {
+                "employeeNumber": 263,
+                "name": "Nitin Yadav",
+                "department": "Physics",
+                "designation": "Assistant Professor"
+            },
+            {
+                "employeeNumber": 164,
+                "name": "Renu",
+                "department": "Chemistry",
+                "designation": "Assistant Professor"
+            },
+            {
+                "employeeNumber": 203,
+                "name": "Preety Yadav",
+                "department": "Chemistry",
+                "designation": "Assistant Professor"
+            },
+            {
+                "employeeNumber": 261,
+                "name": "Harshika",
+                "department": "Chemistry",
+                "designation": "Assistant Professor"
+            },
+            {
+                "employeeNumber": 209,
+                "name": "Moni",
+                "department": "Zoology",
+                "designation": "Assistant Professor"
+            },
+            {
+                "employeeNumber": 17,
+                "name": "Rachana Yadav",
+                "department": "Zoology",
+                "designation": "Assistant Professor"
+            },
+            {
+                "employeeNumber": 123,
+                "name": "Jyoti",
+                "department": "Zoology",
+                "designation": "Assistant Professor"
+            },
+            {
+                "employeeNumber": 143,
+                "name": "Ashok Kumar",
+                "department": "Zoology",
+                "designation": "Assistant Professor"
+            },
+            {
+                "employeeNumber": 264,
+                "name": "Supriya",
+                "department": "Botany",
+                "designation": "Assistant Professor"
+            },
+            {
+                "employeeNumber": 240,
+                "name": "Sunita",
+                "department": "Mathematics",
+                "designation": "Assistant Professor"
+            },
+            {
+                "employeeNumber": 134,
+                "name": "Puja Kumari",
+                "department": "Mathematics",
+                "designation": "Assistant Professor"
+            },
+            {
+                "employeeNumber": 128,
+                "name": "Rambharosh",
+                "department": "Mathematics",
+                "designation": "Assistant Professor"
+            },
+            {
+                "employeeNumber": 76,
+                "name": "Lalit",
+                "department": "Mathematics",
+                "designation": "Assistant Professor"
+            },
+            {
+                "employeeNumber": 207,
+                "name": "Jyoti",
+                "department": "Commerce",
+                "designation": "Assistant Professor"
+            },
+            {
+                "employeeNumber": 208,
+                "name": "Alka Yadav",
+                "department": "Commerce",
+                "designation": "Assistant Professor"
+            },
+            {
+                "employeeNumber": 262,
+                "name": "Jyoti",
+                "department": "Commerce",
+                "designation": "Assistant Professor"
+            },
+            {
+                "employeeNumber": 238,
+                "name": "Monu",
+                "department": "Computer Science",
+                "designation": "Assistant Professor"
+            },
+            {
+                "employeeNumber": 239,
+                "name": "Nisha",
+                "department": "Computer Science",
+                "designation": "Assistant Professor"
+            },
+            {
+                "employeeNumber": 60,
+                "name": "Amit Kumar Sharma",
+                "department": "Hindi",
+                "designation": "Assistant Professor"
+            },
+            {
+                "employeeNumber": 117,
+                "name": "Mahender Yadav",
+                "department": "English",
+                "designation": "Assistant Professor"
+            },
+            {
+                "employeeNumber": 266,
+                "name": "Monika Rohilla",
+                "department": "English",
+                "designation": "Assistant Professor"
+            },
+            {
+                "employeeNumber": 158,
+                "name": "Manjeet",
+                "department": "Geography",
+                "designation": "Assistant Professor"
+            },
+            {
+                "employeeNumber": 227,
+                "name": "Sandeep Kumar",
+                "department": "Geography",
+                "designation": "Assistant Professor"
+            },
+            {
+                "employeeNumber": 216,
+                "name": "Nitesh",
+                "department": "Political Science",
+                "designation": "Assistant Professor"
+            },
+            {
+                "employeeNumber": 267,
+                "name": "Parikshit",
+                "department": "Political Science",
+                "designation": "Assistant Professor"
+            },
+            {
+                "employeeNumber": 265,
+                "name": "Neetu",
+                "department": "Political Science",
+                "designation": "Assistant Professor"
+            },
+            {
+                "employeeNumber": 205,
+                "name": "Sunil Kumar",
+                "department": "History",
+                "designation": "Assistant Professor"
+            },
+            {
+                "employeeNumber": 59,
+                "name": "Sudesh",
+                "department": "PTI",
+                "designation": "PTI"
+            },
+            {
+                "employeeNumber": 108,
+                "name": "Vinay Kumar",
+                "department": "Reasoning",
+                "designation": "Assistant Professor"
+            },
+            {
+                "employeeNumber": 16,
+                "name": "Laxmi Bai",
+                "department": "Librarian",
+                "designation": "Librarian"
+            },
+            {
+                "employeeNumber": 129,
+                "name": "Raghunandan",
+                "department": "Lab Attendant",
+                "designation": "Lab Attendent"
+            },
+            {
+                "employeeNumber": 115,
+                "name": "Pradeep Yadav",
+                "department": "Admin",
+                "designation": "Principal"
+            },
+            {
+                "employeeNumber": 144,
+                "name": "Deepak Kumar",
+                "department": "Admin",
+                "designation": "Admin"
+            },
+            {
+                "employeeNumber": 21,
+                "name": "Babrubhan",
+                "department": "Admin",
+                "designation": "Principal"
+            },
+            {
+                "employeeNumber": 22,
+                "name": "Manjeet Kumar",
+                "department": "Admin",
+                "designation": "Admin"
+            },
+            {
+                "employeeNumber": 268,
+                "name": "Manoj Kumar",
+                "department": "Design",
+                "designation": "Designer"
+            },
+            {
+                "employeeNumber": 269,
+                "name": "Adrash",
+                "department": "Design",
+                "designation": "Designer"
+            },
+            {
+                "employeeNumber": 270,
+                "name": "Sudrashan",
+                "department": "4th Class",
+                "designation": "Peon"
+            },
+            {
+                "employeeNumber": 73,
+                "name": "Ashwin Kumar",
+                "department": "Accountant",
+                "designation": "Accountant"
+            }
+        ]
+    }
+
+
+    ]
+export default json;
--- src/app/(main)/test2/page.js
+++ src/app/(main)/test2/page.js
@@ -1,27 +1,27 @@
 import React from 'react'
 import '@/utils/yo.css'
-import json from './data'
+import json from './faclty'
 
 export default ({ }) => {
 
-
+    console.log(json)
 
     return (
 
         <div className="w-full flex flex-row justify-center items-center flex-wrap  gap-4 p-2 ">
             {
-                json.map((r) => (
+                json[0].faculty.map((r) => (
                     <div className='w-44 h-auto rounded-sm overflow-hidden topper-student-shodow'>
                         <div className='w-full  overflow-hidden  w-80'>
                             <img
                                 className='w-full h-full object-contain'
-                                src={`https://admin.yaduvanshigroup.edu.in/uploads/college-top/top-10-pg/${r.scan_index}.jpg`}
+                                src={'https://admin.yaduvanshigroup.edu.in/uploads/branch-file/college/ydcmgh/staff-photo/'+r.employeeNumber+".j"}
                                 alt="College Top"
                             />                        </div>
                         <div className='text-center py-2'>
                             <h1>{r.name}</h1>
-                            <h1>RANK : {r.rank}</h1>
-                            <h2>{r.class}</h2>
+                            <h1> {r.department}</h1>
+                            <h2>ID: {r.employeeNumber}</h2>
                         </div>
                     </div>
                 ))
